8b0233b7662e603a9635210008ad9ea6fb4b569b
gürkan şahin <33648840+gurkansahin28@users.noreply.github.com>
2024-09-24 22:39:14 +0300

    basic_variables.cpp

---
+++ basic_variables.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using std::cout, std::endl, std::string;
+
+int main(){
+    int age = 55;
+    string message = "Hello again!";
+    const double Pi = 3.14;
+    char letter = 'A';
+    bool isRainny = false;
+
+    cout << age << endl;
+    cout << message << endl;
+    cout << Pi << endl;
+    cout << letter << endl;
+    cout << isRainny << endl;
+    return 0;
+}
